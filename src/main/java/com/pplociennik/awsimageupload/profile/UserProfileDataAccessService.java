1945280b564a11b0a9d30e6928ed59d0a59c3d32
pplociennik23 <pplociennik23@gmail.com>
2023-10-20 13:31:32 +0200

    [05] - Added getUserProfiles API endpoint, Business Logic Service and DataAccess Service

---
+++ src/main/java/com/pplociennik/awsimageupload/profile/UserProfileDataAccessService.java
@@ -0,0 +1,22 @@
+package com.pplociennik.awsimageupload.profile;
+
+import com.pplociennik.awsimageupload.datastore.FakeUserProfileDataStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserProfileDataAccessService {
+
+    private final FakeUserProfileDataStore fakeUserProfileDataStore;
+
+    @Autowired
+    public UserProfileDataAccessService(FakeUserProfileDataStore fakeUserProfileDataStore) {
+        this.fakeUserProfileDataStore = fakeUserProfileDataStore;
+    }
+
+    public List<UserProfile> getUserProfiles(){
+        return fakeUserProfileDataStore.getUserProfiles();
+    }
+}
