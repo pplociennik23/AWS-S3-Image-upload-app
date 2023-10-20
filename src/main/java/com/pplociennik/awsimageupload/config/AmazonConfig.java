fe2689aeda9e8b6b157c8dc7fba99027105f9064
pplociennik23 <pplociennik23@gmail.com>
2023-10-20 13:29:44 +0200

    [04] - Changes to S3 configuration and Main class renamed

--- src/main/java/com/pplociennik/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/pplociennik/awsimageupload/config/AmazonConfig.java
@@ -3,6 +3,7 @@ package com.pplociennik.awsimageupload.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ public class AmazonConfig {
 
         return AmazonS3ClientBuilder
             .standard()
+            .withRegion(Regions.EU_CENTRAL_1)
             .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
             .build();
     }
