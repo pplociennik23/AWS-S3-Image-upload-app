c3ded4efa1fbe01cd8d6ac8ed44ce69cf12889fd
pplociennik23 <pplociennik23@gmail.com>
2023-10-30 22:54:33 +0100

    [11] - Added React-Dropzone component and changed background color

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,5 +1,6 @@
 
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import {useDropzone} from 'react-dropzone'
 import './App.css';
 import axios from 'axios';
 
@@ -24,13 +25,34 @@ const UserProfiles = () => {
 
     return (
     <div key={index}>
+      <br/>
       <h1>{userProfile.userName}</h1>
       <p>{userProfile.userProfileId}</p>
+      <Dropzone/>
+      <br/>
     </div>
     )
   })
 }
 
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag profile image here, or click to add file</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
