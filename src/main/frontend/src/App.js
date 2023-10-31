4ccbd373643fb633d4a49700e46a0cf0d2905491
pplociennik23 <pplociennik23@gmail.com>
2023-10-31 11:33:36 +0100

    [13] - fixed file max size bug and deleted obsolete tags in index.html

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -28,18 +28,34 @@ const UserProfiles = () => {
       <br/>
       <h1>{userProfile.userName}</h1>
       <p>{userProfile.userProfileId}</p>
-      <Dropzone/>
+      <Dropzone {...userProfile}/>
       <br/>
     </div>
     )
   })
 }
 
-function Dropzone() {
+function Dropzone({userProfileId}) {
   const onDrop = useCallback(acceptedFiles => {
+   
     const file = acceptedFiles[0];
     console.log(file);
-  }, [])
+    
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post(`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,formData,{
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }).then(() => {
+      console.log("file uploaded succesfully");
+    }).catch(err => {
+      console.log(err);
+    });
+
+  }, []);
+
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
